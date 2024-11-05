e743146d93ac3a08ad693f9cea4d6e4b0b6b4768
lynickyang <likesheep@outlook.com>
2024-11-06 00:05:15 +0800

    update all

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter ,Input,Output} from '@angular/core';
+import {type User} from './user.model';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+
+
+
+export class UserComponent {
+  @Input({required:true}) user!:User;
+  @Output()select= new EventEmitter();
+  @Input({required:true})selected!:boolean;
+
+  get imagePath(){
+    return 'assets/users/' + this.user.avatar;
+  }
+  onSelectUser(){
+    this.select.emit(this.user.id);
+    console.log("點擊emitid")
+  }
+}
+
